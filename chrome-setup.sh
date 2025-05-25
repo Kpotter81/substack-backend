f0892ea99f93ec2199beba2eb93159d72735ff2d
Kpotter81 <kpotter.fiction@gmail.com>
2025-05-25 09:19:07 -0500

    Update and rename cc to chrome-setup.sh

---
+++ chrome-setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Install dependencies
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Installing Chrome dependencies..."
+apt-get update -yqq
+apt-get install -yqq \
+    wget \
+    curl \
+    gnupg \
+    ca-certificates \
+    fonts-liberation \
+    libappindicator3-1 \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libcups2 \
+    libdbus-1-3 \
+    libgdk-pixbuf2.0-0 \
+    libnspr4 \
+    libnss3 \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    xdg-utils \
+    --install-suggests
+
+# Install Chrome
+echo "Downloading Chrome..."
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt-get update -yqq
+apt-get install -yqq google-chrome-stable
+
+# Clean up
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# NOTE: If this file is in the root folder,
+# your Render build command must be:
+# ./chrome-setup.sh && yarn install
+
