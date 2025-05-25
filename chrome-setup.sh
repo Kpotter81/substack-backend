3f6eae34a7f953bd463ed3e7ecc56fbc13a9d17a
Kpotter81 <kpotter.fiction@gmail.com>
2025-05-25 09:40:49 -0500

    Update chrome-setup.sh

--- chrome-setup.sh
+++ chrome-setup.sh
@@ -3,7 +3,6 @@
 # Install dependencies
 export DEBIAN_FRONTEND=noninteractive
 
-echo "Installing Chrome dependencies..."
 apt-get update -yqq
 apt-get install -yqq \
     wget \
@@ -27,18 +26,12 @@ apt-get install -yqq \
     xdg-utils \
     --install-suggests
 
-# Install Chrome
-echo "Downloading Chrome..."
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 apt-get update -yqq
 apt-get install -yqq google-chrome-stable
 
-# Clean up
+which google-chrome-stable || echo "❌ Chrome install failed"
+
 apt-get clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# NOTE: If this file is in the root folder,
-# your Render build command must be:
-# ./chrome-setup.sh && yarn install
-
